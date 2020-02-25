d208765b7b9490f420344af74fff7488d88ba44c
huan.ren <huan.ren@ly.com>
2020-02-25 15:05:10 +0800

    2/25

--- handleTime.go
+++ handleTime.go
@@ -6,7 +6,8 @@ import "time"
 
 //type location time.Local
 var location=time.Local
-
+const  timeLayout = "2006-01-02 15:04:05"
+const timeDate  = "2006-01-02"
 //设置相应的时区
 func GetLocation(local string){
 	location,_ = time.LoadLocation(local)
@@ -15,22 +16,34 @@ func GetLocation(local string){
 //时间格式转化为相应的日期格式,如传入time.now()
 func TimeToYYYYMMDDHHMMSS(t time.Time)string{
 	//loc,_:=time.LoadLocation("Asia/Beijing")
-	timeNow:=t.Format("2006-01-02 15:04:05")
+	timeNow:=t.Format(timeLayout)
 	return timeNow
 }
 
 func TimeToYYYYMMDD(t time.Time) string{
-	timeNow:=t.Format("2006-01-02")
+	timeNow:=t.Format(timeDate)
 	return timeNow
 }
 
 //时间戳转化为时间
-func TimeStampToTime(t int64){
-
+func TimeStampToTime(t int64)string{
+	datetime := time.Unix(t, 0).Format(timeLayout)
+	return datetime
 }
 
 //string格式转化为时间戳格式
-func GetTimeStamp(str string)int64{
-	return 1
+func StringToYYYYMMDD(str string)(time.Time,error){
+	return time.Parse(timeDate,str)
 }
 
+func StringToYYYYMMDDHHMMSS(str string)(time.Time,error){
+	return time.Parse(timeLayout,str)
+}
+
+//时间差
+func TimeSub(t1 int64,t2 int64)string{
+	time1:= time.Unix(t1,0)
+	time2:=time.Unix(t2,0)
+	subTime:= time1.Sub(time2)
+
+}
